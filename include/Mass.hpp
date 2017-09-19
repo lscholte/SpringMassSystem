2419140caeff52de1728030c0090facaff7bc7b0
Liam Scholte <lscholte@uvic.ca>
2017-09-19 00:04:54 -0700

    Reduces accessibility of Coil and Mass buffer arrays from public to private

--- include/Mass.hpp
+++ include/Mass.hpp
@@ -14,14 +14,14 @@ class Mass : public atlas::utils::Geometry
 		void renderGeometry(atlas::math::Matrix4 const &projection, atlas::math::Matrix4 const &view) override;
 		void updateGeometry(atlas::core::Time<> const &t) override;
 	
+	private:
+		GLuint mVao;
+		GLuint mPositionBuffer, mNormalBuffer, mColorBuffer, mIndexBuffer;
+	
 		static const GLfloat POSITIONS[][3];
 		static const GLfloat NORMALS[][3];
 		static const GLfloat COLORS[][3];
 		static const GLint INDICES[][3];
-	
-	private:
-		GLuint mVao;
-		GLuint mPositionBuffer, mNormalBuffer, mColorBuffer, mIndexBuffer;
 };
 
 #endif
